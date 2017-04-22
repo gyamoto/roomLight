2d2bb6c52c20de146b56717d671fe446c983e77c
gya <farundorlnightray@gmail.com>
2017-04-23 00:35:30 +0900

    fix send only

--- homeIr.ino
+++ homeIr.ino
@@ -1,160 +1,55 @@
 #include "types.h"
-  
+
 //========================================================
 // Config
 //========================================================
-  
-#define SERIAL_BPS             (57600)   /* Serial bps */
-#define IR_IN                  (8)       /* Input      */
-#define IR_OUT                 (13)      /* Output     */
-  
-#define TIMEOUT_RECV_NOSIGNAL  (50000)
-#define TIMEOUT_RECV           (5000000)
-#define TIMEOUT_SEND           (2000000)
-  
-//========================================================
-// Define
-//========================================================
-#define STATE_NONE             (-1)
-#define STATE_OK               (0)
-#define STATE_TIMEOUT          (1)
-#define STATE_OVERFLOW         (2)
-#define DATA_SIZE              (800)
-  
+#define IR_OUT                (13)
+#define LED_OUT               (12)
+#define SW_ON                 (3)
+#define SW_OFF                (2)
+
+#define DATA_SIZE             (67)
+
 //========================================================
 // Program
 //========================================================
-u2 data[DATA_SIZE];
-  
-void sendSignal(){
-  u1 x;
-  s1 state = STATE_NONE;
+u2 PATTERN_ON[67] = { 8155, 4619, 516, 1729, 517, 653, 485, 632, 504, 661, 473, 655, 483, 621, 515, 653, 483, 648, 495, 651, 485, 651, 488, 648, 487, 660, 473, 1733, 517, 656, 483, 1728, 517, 1728, 523, 1719, 527, 661, 476, 653, 485, 660, 476, 660, 475, 1729, 519, 655, 484, 655, 487, 661, 476, 1731, 520, 1728, 520, 1731, 517, 1728, 519, 664, 473, 1727, 521, 1713, 527 };
+u2 PATTERN_OFF[67] = { 8123, 4591, 548, 1719, 517, 669, 471, 668, 465, 652, 487, 668, 468, 667, 468, 671, 468, 669, 471, 668, 471, 667, 467, 669, 468, 668, 471, 1745, 503, 676, 460, 1741, 507, 1749, 504, 1737, 511, 1747, 501, 669, 471, 675, 461, 668, 468, 1744, 504, 667, 471, 681, 460, 665, 469, 665, 468, 1747, 501, 1748, 501, 1745, 504, 668, 465, 1745, 508, 1745, 503 };
+
+void sendSignal(u2 data[]) {
   u2 time = 0;
-  u4 tmp = 0;
-  u4 index = 0;
+  u4 index = DATA_SIZE;
   u4 count = 0;
-  u4 us = 0;
-    
-  us = micros();
-    
-  while(state == STATE_NONE){
-    if(Serial.available() == 0){
-      if((micros() - us) > TIMEOUT_SEND){
-        state = STATE_TIMEOUT;
-        break;
-      }
-    } else {
-      x = Serial.read();
-      if(x>='0' && x<='9'){
-        /* 数字を受信した場合 */
-        tmp *= 10;
-        tmp += x - '0';
-      } else {
-        /* 数字以外を受信した場合 */
-        if((tmp == 0) && (index == 0)){
-          /* 最初の一文字目は読み飛ばす */
-        } else {
-          data[index] = (u2)tmp;
-          if(tmp == 0){
-            state = STATE_OK;
-            break;
-          } else if(index >= DATA_SIZE){
-            state = STATE_OVERFLOW;
-            break;
-          }
-          index++;
-        }
-        tmp = 0;
-      }
-    }
+  u4 us = micros();
+
+  for (count = 0; count < index; count++) {
+    time = data[count];
+    us = micros();
+    do {
+      digitalWrite(IR_OUT, !(count & 1));
+      delayMicroseconds(8);
+      digitalWrite(IR_OUT, 0);
+      delayMicroseconds(7);
+    } while (s4(us + time - micros()) > 0);
   }
-  
-  if(state == STATE_OK){
-    for(count = 0; count < index; count++){
-      time = data[count];
-      us = micros();
-      do {
-        digitalWrite(IR_OUT, !(count&1));
-        delayMicroseconds(8);
-        digitalWrite(IR_OUT, 0);
-        delayMicroseconds(7);
-      }while(s4(us + time - micros()) > 0);
-    }
-    Serial.println("OK");
-  } else {
-    Serial.print("NG:");
-    Serial.println(state);
-  }
+
+  digitalWrite(LED_OUT, HIGH);
+  delay(300);
+  digitalWrite(LED_OUT, LOW);
+  delay(700);
 }
-  
-void recvSignal(){
-    
-  u1 pre_value = HIGH;
-  u1 now_value = HIGH;
-  u1 wait_flag = TRUE;
-  s1 state = STATE_NONE;
-  u4 pre_us = micros();
-  u4 now_us = 0;
-  u4 index = 0;
-  u4 i = 0;
-    
-  while(state == STATE_NONE){
-    now_value = digitalRead(IR_IN);
-    if(pre_value != now_value){
-      now_us = micros();
-      if(!wait_flag){
-        data[index++] = now_us - pre_us;
-      }
-      wait_flag = FALSE;
-      pre_value = now_value;
-      pre_us = now_us;
-    }
-      
-    if(wait_flag){
-        if((micros() - pre_us) > TIMEOUT_RECV){
-          state = STATE_TIMEOUT;
-          break;
-        }
-      } else {
-        if((micros() - pre_us) > TIMEOUT_RECV_NOSIGNAL){
-          state = STATE_OK;
-          break;
-        }
-      }
-  }
-    
-  if(state == STATE_OK){
-    Serial.print("s,");
-    for(i = 0; i<index; i++){
-      Serial.print(data[i]);
-      Serial.print(',');
-    }
-    Serial.println("0,");
-  } else {
-    Serial.println("NG");
-  }
-}
-  
-void setup(){
-  Serial.begin(SERIAL_BPS);
-  pinMode(IR_IN, INPUT);
+
+void setup() {
   pinMode(IR_OUT, OUTPUT);
+  pinMode(SW_ON, INPUT_PULLUP);
+  pinMode(SW_OFF, INPUT_PULLUP);
 }
-  
-void loop(){
-  u1 x;
-  if(Serial.available()){
-    x=Serial.read();
-    switch(x){
-      case 's':
-        sendSignal();
-        break;
-      case 'r':
-        recvSignal();
-        break;
-      default:
-        break;
+
+void loop() {
+  if (digitalRead(SW_ON) == LOW) {
+      sendSignal(PATTERN_ON);
+    } else if (digitalRead(SW_OFF) == LOW) {
+      sendSignal(PATTERN_OFF);
     }
-  }
 }
 
